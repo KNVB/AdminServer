37efb414b96cbc7de69dad345aee8829fc26c68a
cstsang <cstsang@notebook>
2017-12-27 22:07:03 +0800

    2017-12-27

--- AdminClient/js/ui/ServerPage.js
+++ AdminClient/js/ui/ServerPage.js
@@ -37,17 +37,23 @@ class ServerPage
 	}
 	buildAddServerPage(serverResponseObj)
 	{
+		var passiveModeDetail=$(document.createElement("div"));
 		var ipList=serverResponseObj.returnObjects["localIpList"];
-		var temp=$(document.createElement("fieldset"));
-		var table=document.createElement("table");
+		var fieldSet=$(document.createElement("fieldset"));
+		var bindingTable=document.createElement("table");
 		var select=$(document.createElement("select"));
+		var table=document.createElement("table");
 		var row=table.insertRow(table.rows.length);
-		var cell=row.insertCell(0);
+		var cell=row.insertCell(row.length);
+		cell.innerHTML="Description:<input type=text required id=\"desc\">";
+		fieldSet.append("<legend>Binding</legend>");
+		row=bindingTable.insertRow(bindingTable.rows.length);
+		cell=row.insertCell(cell.length);
 		cell.innerHTML="IP address:";
 		cell=row.insertCell(1);
 		cell.innerHTML="Port:";
-		row=table.insertRow(table.rows.length);
-		cell=row.insertCell(0);
+		row=bindingTable.insertRow(bindingTable.rows.length);
+		cell=row.insertCell(row.cells.length);
 		select.attr("id","bindAddress");
 		select.append("<option value=\"*\">All Unassigned</option>");
 		for (var i=0;i<ipList.length;i++)
@@ -55,16 +61,44 @@ class ServerPage
 			select.append("<option value=\""+ipList[i]+"\">"+ipList[i]+"</option>");
 		}		
 		$(cell).append(select);
-		cell=row.insertCell(1);
+		cell=row.insertCell(row.cells.length);
 		
 		$(cell).append("<input type=\"number\" required id=\"port\" min=\"1\" max=\"65535\" value=\"21\">");
-		temp.append("<legend>Binding</legend>");
-		temp.append(table);
-		this.mainStage.append(temp);
-		this.mainStage.append("<br>");		
-		temp=$(document.createElement("fieldset"));
-		temp.css("border","none");
-		temp.append("<legend><input type=\"checkbox\" id=\"isPassiveMode\">Passive mode</legend>");
-		this.mainStage.append(temp);
+		fieldSet.append(bindingTable);
+		
+		row=table.insertRow(table.rows.length);
+		cell=row.insertCell(row.cells.length);
+		$(cell).append(fieldSet);
+
+		row=table.insertRow(table.rows.length);
+		cell=row.insertCell(row.cells.length);
+		fieldSet=$(document.createElement("fieldset"));
+		fieldSet.attr("id","passiveModeSetting");
+		fieldSet.css("border","none");
+		fieldSet.append("<legend><input type=\"checkbox\" id=\"isPassiveMode\">Support Passive mode</legend>");
+		passiveModeDetail.attr("id","passiveModeDetail"); 
+		passiveModeDetail.css("display","none");
+		passiveModeDetail.append("Port Range:<input type=text id=\"portRange\" required>");
+		fieldSet.append(passiveModeDetail);
+		$(cell).append(fieldSet);
+		$(table).css("height","100%");
+		this.mainStage.append(table);
+		row=table.insertRow(table.rows.length);
+		cell=row.insertCell(row.cells.length);
+		cell.innerHTML="<input type=\"submit\" value=\"add server definition\" style=\"float: right;\">";
+		$("#isPassiveMode").click(function()
+				  {
+					var passiveModeSetting=document.getElementById("passiveModeSetting");
+					var passiveModeDetail=$("#passiveModeDetail");
+					passiveModeDetail.toggle();
+					if (passiveModeSetting.style.border=="none")
+					{
+						passiveModeSetting.style.border="1px solid black";
+					}
+					else
+					{
+						passiveModeSetting.style.border="none";
+					}
+				  });
 	}
 }
